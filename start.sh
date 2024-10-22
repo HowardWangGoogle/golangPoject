e2f7664376cdd718c1b20481a1079ac4e0f50780
Howard <haobowang86@gmail.com>
2024-10-22 18:28:06 +0800

    load

--- start.sh
+++ start.sh
@@ -4,12 +4,6 @@ set -e
 echo "run db migration"
 source "/app/app.env"
 
-DB_SOURCE=postgresql://root:e72ZEDyYtruZfNzNO3r5@simple-bank.clgu0eqaq7ju.ap-southeast-2.rds.amazonaws.com:5432/simple_bank
-DB_DRIVER=postgres
-SERVER_ADDRESS=0.0.0.0:8080
-ACCESS_TOKEN_DURATION=15m
-TOKEN_SYMETRIC_KEY=beee0ad9c7bbd5ebb8bafb47b28ef325
-
 
 # 打印 DB_SOURCE 以调试
 echo "DB_SOURCE: $DB_SOURCE"
