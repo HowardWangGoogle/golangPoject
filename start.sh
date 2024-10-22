6981614007908acfd13141b3c2be0e4c20f821a2
Howard <haobowang86@gmail.com>
2024-10-22 19:26:54 +0800

    load

--- start.sh
+++ start.sh
@@ -3,19 +3,31 @@ set -e
 
 echo "run db migration"
 
-# 假设你已经将 SecretString 存储在 SECRET_STRING 变量中
-SECRET_STRING='{"DB_SOURCE":"postgresql://root:e72ZEDyYtruZfNzNO3r5@simple-bank.clgu0eqaq7ju.ap-southeast-2.rds.amazonaws.com:5432/simple_bank","DB_DRIVER":"postgres","SERVER_ADDRESS":"0.0.0.0:8080","ACCESS_TOKEN_DURATION":"15m","TOKEN_SYMETRIC_KEY":"beee0ad9c7bbd5ebb8bafb47b28ef325"}'
-
-# 使用 jq 提取 DB_SOURCE
-export DB_SOURCE=$(echo $SECRET_STRING | jq -r .DB_SOURCE)
-
-# 检查 DB_SOURCE 是否被正确设置
-if [ -z "$DB_SOURCE" ]; then
-    echo "Error: DB_SOURCE is not set!"
+# 加载环境变量
+if [ ! -f /app/app.env ]; then
+    echo "Error: app.env file not found!"
     exit 1
 fi
+source /app/app.env
 
-echo "DB_SOURCE: $DB_SOURCE"
+# 创建 JSON 字符串
+env_json=$(jq -n \
+    --arg db_source "$DB_SOURCE" \
+    --arg db_driver "$DB_DRIVER" \
+    --arg server_address "$SERVER_ADDRESS" \
+    --arg access_token_duration "$ACCESS_TOKEN_DURATION" \
+    --arg token_symetric_key "$TOKEN_SYMETRIC_KEY" \
+    '{
+        DB_SOURCE: $db_source,
+        DB_DRIVER: $db_driver,
+        SERVER_ADDRESS: $server_address,
+        ACCESS_TOKEN_DURATION: $access_token_duration,
+        TOKEN_SYMETRIC_KEY: $token_symetric_key
+    }')
+
+# 输出 JSON
+echo "Extracted Environment Variables in JSON format:"
+echo "$env_json"
 
 # 运行迁移
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
