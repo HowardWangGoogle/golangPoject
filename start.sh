185b7c610cc665a44f95d6a60a5b1331588a6b14
Howard <haobowang86@gmail.com>
2024-10-22 19:15:52 +0800

    load

--- start.sh
+++ start.sh
@@ -2,17 +2,19 @@
 set -e
 
 echo "run db migration"
-source /app/app.env
 
-if [ ! -f /app/app.env ]; then
-    echo "Error: app.env file not found!"
+# 假设你已经将 SecretString 存储在 SECRET_STRING 变量中
+SECRET_STRING='{"DB_SOURCE":"postgresql://root:e72ZEDyYtruZfNzNO3r5@simple-bank.clgu0eqaq7ju.ap-southeast-2.rds.amazonaws.com:5432/simple_bank","DB_DRIVER":"postgres","SERVER_ADDRESS":"0.0.0.0:8080","ACCESS_TOKEN_DURATION":"15m","TOKEN_SYMETRIC_KEY":"beee0ad9c7bbd5ebb8bafb47b28ef325"}'
+
+# 使用 jq 提取 DB_SOURCE
+export DB_SOURCE=$(echo $SECRET_STRING | jq -r .DB_SOURCE)
+
+# 检查 DB_SOURCE 是否被正确设置
+if [ -z "$DB_SOURCE" ]; then
+    echo "Error: DB_SOURCE is not set!"
     exit 1
 fi
 
-cat /app/app.env
-
-
-# 打印 DB_SOURCE 以调试
 echo "DB_SOURCE: $DB_SOURCE"
 
 # 运行迁移
